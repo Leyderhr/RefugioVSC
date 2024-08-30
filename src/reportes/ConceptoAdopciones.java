2a9f58b165a0c5804e11d98eecaf65f3f0cad05e
Leyderhr <165097459+Leyderhr@users.noreply.github.com>
2024-08-30 14:39:49 -0400

    Actualizaciones. Corregidos errores generales y agregado la opción de guardar como pdf en las pantallas de reporte

--- src/reportes/ConceptoAdopciones.java
+++ src/reportes/ConceptoAdopciones.java
@@ -7,6 +7,12 @@ import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Objects;
 import com.spire.doc.Document;
 import com.spire.doc.FileFormat;
@@ -27,29 +33,61 @@ public class ConceptoAdopciones {
         JasperViewer view = new JasperViewer(jprint, false);
         view.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         view.setVisible(true);
-        view.getContentPane().add(new JButton("Casa"));
+
+        view.getContentPane().setLayout(null);
+
+        view.getContentPane().getComponent(0).getComponentAt(0,0).getComponentAt(0,0).getComponentAt(0,2).setEnabled(false);
+        view.getContentPane().getComponent(0).getComponentAt(0,0).getComponentAt(0,0).getComponentAt(0,2).setVisible(false);
+        view.getContentPane().addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if(e.getClickCount() == 2 && view.getContentPane().contains(e.getPoint())) {
+                    try {
+                        if(JOptionPane.showConfirmDialog(null, "¿Desea guardar el reporte como pdf?", "Guardar", JOptionPane.OK_CANCEL_OPTION) == JOptionPane.OK_OPTION){
+                            JFileChooser fileChooser = new JFileChooser();
+                            fileChooser.setDialogTitle("Guardar reporte");
+                            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                            FileNameExtensionFilter filtroPdf = new FileNameExtensionFilter("Archivos pdf", "pdf");
+
+                            // Agregar el filtro al JFileChooser
+                            fileChooser.addChoosableFileFilter(filtroPdf);
+
+                            if(fileChooser.showDialog(view, "Guardar") == JFileChooser.APPROVE_OPTION)
+                                exportarPDF_DOC(fileChooser.getSelectedFile());
+
+                        }
+
+                    } catch (JRException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            }
+        });
+        JOptionPane.showMessageDialog(null, "Para guardar el reporte de doble click en la barra " +
+                "de herramientas", "Advertencia", JOptionPane.WARNING_MESSAGE);
 
     }
 
-    public void exportarPDF_DOC() throws JRException {
+    public void exportarPDF_DOC(File currentD) throws JRException {
 
         JRDocxExporter exporter = new JRDocxExporter();
         exporter.setParameter(JRExporterParameter.JASPER_PRINT, jprint);
-        exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, "ConceptoAdopciones.docx");
+        //exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, "ConceptoAdopciones.docx");
+        exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, String.valueOf(currentD));
         exporter.exportReport();
 
         Document doc = new Document();
 
-        // Load a Word document
-        doc.loadFromFile("ConceptoAdopciones.docx");
+        doc.loadFromFile(String.valueOf(currentD));
+        doc.saveToFile(String.valueOf(currentD));
 
-        // Save the document to PDF
-        doc.saveToFile("ConceptoAdopcionesPDF.pdf", FileFormat.PDF);
+        try {
+            Desktop.getDesktop().open(currentD);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         // Dispose resources
         doc.dispose();
-
-
-
     }
 }
--- src/reportes/ContratosProveedoresAlimentos.java
+++ src/reportes/ContratosProveedoresAlimentos.java
@@ -9,6 +9,12 @@ import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 
 public class ContratosProveedoresAlimentos {
@@ -25,22 +31,58 @@ public class ContratosProveedoresAlimentos {
         JasperViewer view = new JasperViewer(jprint, false);
         view.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         view.setVisible(true);
+
+        view.getContentPane().setLayout(null);
+
+        view.getContentPane().getComponent(0).getComponentAt(0,0).getComponentAt(0,0).getComponentAt(0,2).setEnabled(false);
+        view.getContentPane().getComponent(0).getComponentAt(0,0).getComponentAt(0,0).getComponentAt(0,2).setVisible(false);
+        view.getContentPane().addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if(e.getClickCount() == 2 && view.getContentPane().contains(e.getPoint())) {
+                    try {
+                        if(JOptionPane.showConfirmDialog(null, "¿Desea guardar el reporte como pdf?", "Guardar", JOptionPane.OK_CANCEL_OPTION) == JOptionPane.OK_OPTION){
+                            JFileChooser fileChooser = new JFileChooser();
+                            fileChooser.setDialogTitle("Guardar reporte");
+                            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                            FileNameExtensionFilter filtroPdf = new FileNameExtensionFilter("Archivos pdf", "pdf");
+
+                            // Agregar el filtro al JFileChooser
+                            fileChooser.addChoosableFileFilter(filtroPdf);
+
+                            if(fileChooser.showDialog(view, "Guardar") == JFileChooser.APPROVE_OPTION)
+                                exportarPDF_DOC(fileChooser.getSelectedFile());
+
+                        }
+
+                    } catch (JRException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            }
+        });
+        JOptionPane.showMessageDialog(null, "Para guardar el reporte de doble click en la barra " +
+                "de herramientas", "Advertencia", JOptionPane.WARNING_MESSAGE);
     }
 
-    public void exportarPDF_DOC() throws JRException {
+    public void exportarPDF_DOC(File currentD) throws JRException {
 
         JRDocxExporter exporter = new JRDocxExporter();
         exporter.setParameter(JRExporterParameter.JASPER_PRINT, jprint);
-        exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, "ContratosProveedoresAlimentos.docx");
+        //exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, "ConceptoAdopciones.docx");
+        exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, String.valueOf(currentD));
         exporter.exportReport();
 
         Document doc = new Document();
 
-        // Load a Word document
-        doc.loadFromFile("ContratosProveedoresAlimentos.docx");
+        doc.loadFromFile(String.valueOf(currentD));
+        doc.saveToFile(String.valueOf(currentD));
 
-        // Save the document to PDF
-        doc.saveToFile("ContratosProveedoresAlimentosPDF.pdf", FileFormat.PDF);
+        try {
+            Desktop.getDesktop().open(currentD);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         // Dispose resources
         doc.dispose();
